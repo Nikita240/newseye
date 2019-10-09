d807aea57b56e16baa7980349d72868058a435c5
Sumedh Bhattacharya <sumedhb1995@gmail.com>
2019-10-09 13:39:11 -0700

    splash page setup

--- newseye_frontend/src/App.js
+++ newseye_frontend/src/App.js
@@ -1,18 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
-import {Label} from "@stardust-ui/react";
+import {Button, Image, Text} from "@stardust-ui/react";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Label content={"Test"}/>
+        <Image
+          styles={
+            {
+              width: "30vh"
+            }
+          }
+          circular 
+          src={logo} 
+          className="App-logo" 
+          alt="logo" />
+        <Text 
+          styles={
+            {
+              paddingBottom: "2vh"
+            }
+          }
+          content="Welcome to NewsEye" 
+          size="largest" 
+          weight="bold"
+          />
+        <Button content={"Explore news"}/>
       </header>
+      <div style={{background: "transparent"}}>Icons made by <a href="https://www.flaticon.com/authors/prosymbols" title="Prosymbols">Prosymbols</a> from <a href="https://www.flaticon.com/"             title="Flaticon">www.flaticon.com</a></div>
     </div>
   );
 }
