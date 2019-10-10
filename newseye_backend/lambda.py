4c0e72b4e8b14636ebd3a6d1208a2df217a1d62e
Nikita Rushmanov <rushmanov@roborace.com>
2019-10-09 18:01:10 -0700

    Add /news route

--- newseye_backend/lambda.py
+++ newseye_backend/lambda.py
@@ -13,6 +13,16 @@ def sources(event, context):
         "body": json.dumps(newseye.sources())
     }
 
+def news(event, context):
+
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin" : "*"
+        },
+        "body": json.dumps(newseye.news())
+    }
+
 def news_from_source(event, context):
 
     return {
